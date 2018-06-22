fc00576f8d90631bd6367330be9f586f5f08a3b8
wangtk <wangtk@yangche51.com>
2018-06-22 15:59:36 +0800

    zipkken链路追踪

--- eureka_miya/src/main/java/com/dagemen/HiService.java
+++ eureka_miya/src/main/java/com/dagemen/HiService.java
@@ -1,12 +1,12 @@
 package com.dagemen;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "service-hi")
 public interface HiService {
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
 }
